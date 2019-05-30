f8f9caf2e714d1daf6ef2d0b8e2ca56e9b114532
Luke Arntz <luke@blue42.net>
2019-05-30 13:45:51 -0400

    initial commit

---
+++ Archiver.cs
@@ -0,0 +1,91 @@
+using System.IO.Compression;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+
+namespace zipz
+{
+    public class Archiver
+    {
+        public void CreateArchives(IEnumerable<FileInfo> files, string group)
+        {
+            Dictionary<string,List<FileInfo>> ArchiveFileList = new Dictionary<string, List<FileInfo>>();
+            var archiveDate = DateTime.Now.ToString("s").Replace(":", "_");
+            switch(group)
+            {
+                case "file":
+                    foreach (var file in files)
+                    {
+                        string archiveName = file.DirectoryName + Path.DirectorySeparatorChar.ToString() + file.Name + ".zip";
+                        AddFileToList(ArchiveFileList, archiveName, file);
+                    }
+                    break;
+                case "day":
+                    foreach (var file in files)
+                    {
+                        string archiveName = file.DirectoryName + Path.DirectorySeparatorChar.ToString() + "day-" + 
+                            file.LastWriteTime.ToString("yyyyMMdd") + "-created-" + archiveDate + ".zip";
+                        AddFileToList(ArchiveFileList, archiveName, file);
+                    }
+                    break;
+                case "month":
+                    foreach (var file in files)
+                    {
+                        string archiveName = file.DirectoryName + Path.DirectorySeparatorChar.ToString() + "month-" + 
+                            file.LastWriteTime.ToString("yyyyMM") + "-created-" + archiveDate + ".zip";
+                        AddFileToList(ArchiveFileList, archiveName, file);
+                    }
+                    break;
+                case "year":
+                    foreach (var file in files)
+                    {
+                        string archiveName = file.DirectoryName + Path.DirectorySeparatorChar.ToString() + "year-" +
+                            file.LastWriteTime.ToString("yyyy") + "-created-" + archiveDate + ".zip";
+                        AddFileToList(ArchiveFileList, archiveName, file);
+                    }
+                    break;
+            }
+
+            foreach (var key in ArchiveFileList.Keys)
+            {
+                CompressFiles(key, ArchiveFileList[key]);
+            }
+            System.Console.WriteLine("\n\n");
+        }
+
+        private void CompressFiles(string archivePath, List<FileInfo> files)
+        {
+            int filesAdded = 0;
+            var archiveName = archivePath.Split("\\").Last();            
+            using (FileStream zipToOpen = new FileStream(archivePath, FileMode.CreateNew)) 
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+                {
+                    foreach(FileInfo file in files)
+                    {                        
+                        archive.CreateEntryFromFile(file.FullName, file.Name, CompressionLevel.Optimal);
+                        System.Console.Write(String.Format("\r{0}: {1,7} files compressed.",archiveName,++filesAdded));
+                        File.Delete(file.FullName);
+                    }
+                }
+            }
+            System.Console.WriteLine();
+        }
+        
+        private void AddFileToList(Dictionary<string,List<FileInfo>> list, string key, FileInfo fullname)
+        {
+            if(list.ContainsKey(key))
+            {
+                list[key].Add(fullname);
+            }
+            else
+            {
+                list[key] = new List<FileInfo>();
+                list[key].Add(fullname);
+            }             
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace zipz
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Description = "Zip files in specified directory. \n\n*** SOURCE FILES WILL BE DELETED *** \n\nDefault options `--archive file --extension log`";
+
+            var SourceArg = app.Argument("Path", "Path to directory containing files.").IsRequired().Accepts(v => v.ExistingDirectory());
+            var FileExt = app.Option("-e|--extension <EXT>","File extension to zip. Accepts txt, csv, or log. Example: '-e txt'", CommandOptionType.SingleValue);
+            FileExt.Accepts().Values("txt","csv","log");
+            var ArchivePer = app.Option("-a|--archive","Create one archive per time period. Accepts file, day, month, or year.",CommandOptionType.SingleValue);
+            ArchivePer.Accepts().Values("file","day","month","year");
+            var DateFilter = app.Option("-d|--datefilter","Filter source files by date. Example '--datefilter 2018-03-5' for March 5, 2018. Filter will ",CommandOptionType.SingleValue);
+
+            app.HelpOption();
+            app.OnExecute(() =>
+            {   
+                System.Console.ForegroundColor = ConsoleColor.Cyan;
+                System.Console.WriteLine("\n\n##### ZIPZ #####\n");
+                System.Console.ResetColor();
+
+                string sourceDir = SourceArg.Value;
+                if (!sourceDir.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+                    sourceDir += Path.DirectorySeparatorChar; 
+
+                System.Console.WriteLine("Source: \t{0}",sourceDir);
+
+                string ext = "log";
+                if (FileExt.HasValue())
+                    ext = FileExt.Value();
+                    
+                System.Console.WriteLine("Extension:\t{0}",ext);
+
+                string group = "file";
+                if (ArchivePer.HasValue())
+                    group = ArchivePer.Value();
+
+                System.Console.WriteLine("Archive per: \t{0}",group);
+
+                System.Console.WriteLine("\n\n");
+
+                string searchPattern = "*." + ext;
+                var sourceDirectory = new DirectoryInfo(sourceDir);
+                var files = sourceDirectory.EnumerateFiles(searchPattern);                
+
+                Archiver archiver = new Archiver(); 
+                archiver.CreateArchives(files, group);
+
+            });
+
+            return app.Execute(args);
+        }
+    }
+}
