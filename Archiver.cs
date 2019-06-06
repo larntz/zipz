b29ee1344436617c8e87ec7b2d83f8e9596a9a7d
Luke Arntz <luke@blue42.net>
2019-06-06 11:17:39 -0400

    added handling to update existing archives

--- Archiver.cs
+++ Archiver.cs
@@ -26,7 +26,7 @@ namespace zipz
                     foreach (var file in files)
                     {
                         string archiveName = file.DirectoryName + Path.DirectorySeparatorChar.ToString() + "day-" + 
-                            file.LastWriteTime.ToString("yyyyMMdd") + "-created-" + archiveDate + ".zip";
+                            file.LastWriteTime.ToString("yyyyMMdd") +".zip";
                         AddFileToList(ArchiveFileList, archiveName, file);
                     }
                     break;
@@ -34,7 +34,7 @@ namespace zipz
                     foreach (var file in files)
                     {
                         string archiveName = file.DirectoryName + Path.DirectorySeparatorChar.ToString() + "month-" + 
-                            file.LastWriteTime.ToString("yyyyMM") + "-created-" + archiveDate + ".zip";
+                            file.LastWriteTime.ToString("yyyyMM") +".zip";
                         AddFileToList(ArchiveFileList, archiveName, file);
                     }
                     break;
@@ -42,7 +42,7 @@ namespace zipz
                     foreach (var file in files)
                     {
                         string archiveName = file.DirectoryName + Path.DirectorySeparatorChar.ToString() + "year-" +
-                            file.LastWriteTime.ToString("yyyy") + "-created-" + archiveDate + ".zip";
+                            file.LastWriteTime.ToString("yyyy") +".zip";
                         AddFileToList(ArchiveFileList, archiveName, file);
                     }
                     break;
@@ -58,18 +58,42 @@ namespace zipz
         private void CompressFiles(string archivePath, List<FileInfo> files)
         {
             int filesAdded = 0;
-            var archiveName = archivePath.Split("\\").Last();            
-            using (FileStream zipToOpen = new FileStream(archivePath, FileMode.CreateNew)) 
+            bool existingArchive = false;
+            var archiveName = archivePath.Split("\\").Last();      
+            if (File.Exists(archivePath))
             {
-                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+                File.Move(archivePath, archivePath + ".existing");
+                existingArchive = true;
+            }      
+            using (FileStream zipToOpen = new FileStream(archivePath, FileMode.CreateNew)) 
+            using (ZipArchive newArchive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+            {
+                if(existingArchive)
                 {
-                    System.Console.Write(String.Format("\r{0}: {1,7} files compressed.",archiveName,filesAdded));
-                    foreach(FileInfo file in files)
-                    {                        
-                        archive.CreateEntryFromFile(file.FullName, file.Name, CompressionLevel.Optimal);
-                        System.Console.Write(String.Format("\r{0}: {1,7} files compressed.",archiveName,++filesAdded));
-                        File.Delete(file.FullName);
-                    }
+                    // zip file inception... copying entries from the .existing zip file into a new 
+                    //  archive. *** Doing this so we aren't loading the entire existing archive into RAM (could be yuuuuge!)
+                    // We: 1. open a stream to the existing archive (copyFromArchive), 2. open the zip archive,
+                    //  3. foreach entry in the copyFromArchive we create a new entry in the new archive,
+                    //  4. and read the entry data from existing entry into the new entry.
+                    System.Console.WriteLine("Existing archive found. Copying to new archive...");
+                    using (FileStream existingZipFile = new FileStream(archivePath + ".existing", FileMode.Open, FileAccess.Read))
+                    using (ZipArchive copyFromArchive = new ZipArchive(existingZipFile, ZipArchiveMode.Read))
+                        foreach (var oldEntry in copyFromArchive.Entries)
+                        {                                    
+                            var newEntry = newArchive.CreateEntry(oldEntry.FullName);
+                            using (Stream toStream = newEntry.Open())
+                            using (Stream fromStream = oldEntry.Open())
+                                fromStream.CopyTo(toStream);
+                        }           
+                    File.Delete(archivePath + ".existing");
+                }
+
+                System.Console.Write(String.Format("\r{0}: {1,7} files compressed.",archiveName,filesAdded));
+                foreach(FileInfo file in files)
+                {                        
+                    newArchive.CreateEntryFromFile(file.FullName, file.Name, CompressionLevel.Optimal);
+                    System.Console.Write(String.Format("\r{0}: {1,7} files compressed.",archiveName,++filesAdded));
+                    File.Delete(file.FullName);
                 }
             }
             System.Console.WriteLine();
